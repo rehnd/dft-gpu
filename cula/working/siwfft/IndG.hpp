49886a989681b4f71edfc5eab8298e8cda7326a9
Danny Rehn <danny.rehn@gmail.com>
2014-08-23 17:55:01 -0700

    Adding a file that looks up the index m of \vec G_m through its Miller Indices (i, j, k)

---
+++ cula/working/siwfft/IndG.hpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+class IndG
+{
+  /* This class accesses, for each set of Miller Indices (i, j, k),
+     the corresponding value of m in $\vec G_m$, so that one can find
+     a particular G vector from the Miller Indices. */
+private:
+  int _x, _y, _z; // Array runs from [-_x, _x], [-_y, _y], and [-_z, _z]
+  int _xl, _yl, _zl;
+  int _size;
+  double *a;
+
+public:
+  IndG(int x, int y, int z)
+  {
+    _x = x; _y = y; _z = z;
+    _xl = 2*_x + 1;  _yl = 2*_y + 1;  _zl = 2*_z + 1;
+    _size = _xl*_yl*_zl;
+    printf("size = %d\n", _size);
+    a = (double*)malloc(_size*sizeof(double));
+  }
+  double& operator() (int i, int j, int k)
+  {
+    int index = i + _x + (j + _y)*_xl + (k + _z)*_xl*_yl;
+    if (index < _size && index >= 0)
+      return a[index];
+    else
+    {
+      printf("Error: Out of bounds access for index (%d, %d, %d)\n", i, j, k);
+      exit(1);
+    }
+  }
+  const double& operator() (int i, int j, int k) const
+  {
+    int index = i + _x + (j + _y)*_xl + (k + _z)*_xl*_yl;
+    if (index < _size && index >= 0)
+      return a[index];
+    else
+    {
+      printf("Error: Out of bounds access for index (%d, %d, %d)\n", i, j, k);
+      exit(1);
+    }
+  }
+  ~IndG()
+  {
+    free(a);
+  }
+};
